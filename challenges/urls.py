c6d3cda5bdfa8ff5e0750cbf48ca5015493fa9da
Arun Kaushik <arunkaushikp@gmail.com>
2021-09-16 23:18:33 +0530

    dynamic variable_path

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("",views.home,name="home"), 
-    path("<str:month>",views.month,name="month")   
+    path("",views.home,name="home"),
+    path("<int:month_key>",views.month_number,name="int_month"), 
+    path("<str:month>",views.month,name="str_month")   
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,4 @@
+from typing import KeysView
 from django.http.response import Http404, HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -9,14 +10,34 @@ def home(request):
     return HttpResponse("<h1>Home Page This Is !<h1>")
 
 
+monthly_challenges={
+    "January":"<h1>Jan is the month<h1>",
+    "February":"<h1> February is the month<h1>",
+    "March":"<h1> March is the month<h1>",
+    "April":"<h1> April is the month<h1>",
+    "May":"<h1> May is the month<h1>",
+    "June":"<h1> June is the month<h1>",
+    "July":"<h1> July is the month<h1>",
+    "August":"<h1> August is the month<h1>",
+    "September":"<h1> September is the month<h1>",
+    "October":"<h1> October is the month<h1>",
+    "November":"<h1> November is the month<h1>",
+    "December":"<h1> December is the month<h1>",
+}
+
+def month_number(request,month_key):
+    return_text = None    
+    print(month_key)    
+    values_1=monthly_challenges.values()    
+    value_list = list(values_1)
+    month_value=value_list[month_key-1]
+    return HttpResponse(month_value)
+
 def month(request, month):
     return_text = None
-    if month == "jan" or "1" :
-        return_text = "<h1>Jan is the month<h1>"
-    elif month == "feb":
-        return_text = "<h1>feb is the month<h1>"
-    elif month == "march":
-        return_text = "<h1>march is the month<h1>"
-    else:
+    print(month)
+    try:
+        return_text=monthly_challenges[month]
+    except:
         return HttpResponseNotFound("<h1>Bad request<h1>")
-    return HttpResponse(return_text)
+    return   HttpResponse(return_text)
